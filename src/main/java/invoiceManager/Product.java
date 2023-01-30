c7cd8cbb61ae9f3936361d57cc8d227b64eda0d1
AmeerAllawati <amaab@ucdavis.com>
2023-01-30 11:09:54 +0400

    Added the product class with getters and setters

---
+++ src/main/java/invoiceManager/Product.java
@@ -0,0 +1,46 @@
+package invoiceManager;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private double unitPrice;
+    private int quantity;
+    private double qtyAmountPP;
+    
+    public Product(int id, String name, double unitPrice, int quantity) {
+        // TODO Auto-generated constructor stub
+        this.id = id;
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
+    
+    public void setID(int id) {
+        this.id = id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public void setQtyAmountPP(double qtyAmountPP) {
+        this.qtyAmountPP = qtyAmountPP;
+    }
+    
+    public int getID() {
+        return this.id;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
